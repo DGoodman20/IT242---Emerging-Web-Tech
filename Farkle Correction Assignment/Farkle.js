0ccbaef961a294b6be9266604500b3ebb6ffe429
Danielle Goodman <42615175+DGoodman20@users.noreply.github.com>
2019-03-27 12:52:04 -0400

    Add files via upload

---
+++ Farkle Correction Assignment/Farkle.js
@@ -0,0 +1,59 @@
+class Farkle {
+    constructor() {
+        this._dieCount = 6;
+        this._dieSides = 6;
+        this.dice = [];
+        this.players = [];
+        for (var i = 1; i <= this._dieCount; i++) {
+            this.dice[i - 1] = new Die(this._dieSides);
+        }
+    }
+    addPlayer(playerName) {
+        var newPlayer = new Player();
+        newPlayer.name = playerName;
+        this.players.push(playerName);
+    }
+    drawGame() {
+        document.querySelector('#board').innerHTML = '';
+        this.players.forEach((player) => {
+            this.drawPlayer(player.name);
+        });
+        // loop over the players
+        // display them on the screen
+        // display their score along side their names
+        // draw some kind of a dice interface for viewing current dice status
+    }
+    drawPlayer(player) {
+        var p = document.createElement('div');
+        p.innerText = player;
+        document.querySelector('#board').appendChild(p);
+    }
+}
+{
+    class Player {
+        constructor(name) {
+            this.name = name;
+            this.totalScore = 0;
+            this.currentTurnScore = 0;
+            this.isEndGameStarter = false;
+            this.heldDice = [];
+            for (var i = 1; i <= this.name; i++) {
+                this.isEndGameStarter[i - 1] = new Die(this.heldDice);
+            }
+        }
+        addPlayer(playerName) {
+            this.players.push(playerName);
+        }
+        drawGame() {
+            document.querySelector('#board').innerHTML = '';
+            this.players.forEach((player) => {
+                this.drawPlayer(player);
+            });
+        }
+        drawPlayer(player) {
+            var p = document.createElement('div');
+            p.innerText = player;
+            document.querySelector('#board').appendChild(p);
+        }
+    }
+}
